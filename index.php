f8d45d240dc0a2f3502a92208cad60c8a09a8eac
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-02 23:48:04 +0200

    Exercise continue

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php 
 // inserisci codice
+$text = 'Inserisci la parola da censurare'
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> 
     <title>PHP Badwords</title>
 </head>
     <body>
-        
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-6">
+                    <form action="principal.php" method="POST" class="d-flex flex-column">
+                        <h4><?php echo $text ?></h4>
+                        <input class="mb-2 mt-5" type="text" name="word">
+                        <textarea class="mt-5" name="paragraph" cols="50" rows="10"></textarea>
+                        <button class="btn btn-primary w-25 position-relative top-50 start-50 translate-middle mt-4">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
--- principal.php
+++ principal.php
@@ -1,3 +1,27 @@
 <?php 
-// inserisci codice
+    $paragraph = strtolower($_POST['paragraph']);
+    $word = strtolower($_POST['word']);
+
+    $expolde = explode(" ", $word);
+    $text_final = str_replace($expolde, '***' , $paragraph)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> 
+    <title>PHP Badwords</title>
+</head>
+    <body>
+        <div class="conatiner">
+            <div class="row d-flex justify-content-center">
+                <div class="col-8">
+                    <input type="text" name="word" readonly value="<?php echo $word ?>">
+                    <textarea cols="50" rows="10"><?php echo $text_final ?></textarea>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
